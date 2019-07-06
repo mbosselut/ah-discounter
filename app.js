dbf1469f466ccfa84e366ccaa342258d97821ed5
Manon Bosselut <duchmol7@hotmail.com>
2019-07-06 16:08:16 +0200

    Added today's date

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var request = require('request'),
     express = require("express"),
     app = express(),
-    urlTools = require('url');
+    urlTools = require('url'),
+    date = require('date-and-time');
 
 //tells express to look for ejs files
 app.set("view engine", "ejs");
 //connects to our main css file
 app.use(express.static(__dirname + "/public"));
 
-var productArray = ["https://www.ah.nl/producten/product/wi187593/ah-greenfields-jalapeno-burger", "https://www.ah.nl/producten/product/wi142440/ah-maaltijdsalade-italiaanse-kip", "https://www.ah.nl/producten/product/wi233276/pringles-original", "https://www.ah.nl/producten/product/wi365407/ah-dipsalade-couscous-falafel-met-hummus", "https://www.ah.nl/producten/product/wi62761/dr-oetker-big-americans-pizza-texas", "https://www.ah.nl/producten/product/wi191448/granditalia-piccante-met-rode-peper", "https://www.ah.nl/producten/product/wi112744/pepsi-cola-max-4-pack", "https://www.ah.nl/producten/product/wi457669/beyond-meat-the-beyond-burger", "https://www.ah.nl/producten/product/wi196825/granditalia-fusilli-integrali"];
+var productArray = ["https://www.ah.nl/producten/product/wi187593/ah-greenfields-jalapeno-burger", "https://www.ah.nl/producten/product/wi442669/doritos-cool-american-tortilla-chips", "https://www.ah.nl/producten/product/wi414808/douwe-egberts-lungo-original-koffiecups", "https://www.ah.nl/producten/product/wi185801/ah-duurzame-vangst-sockeye-zalm", "https://www.ah.nl/producten/product/wi211029/robijn-klein-and-krachtig-color-wasmiddel", "https://www.ah.nl/producten/product/wi196835/robijn-wasverzachter-morgenfris", "https://www.ah.nl/producten/product/wi420319/kwekkeboom-oven-black-angus-bitterballen", "https://www.ah.nl/producten/product/wi169797/ah-avocado", "https://www.ah.nl/producten/product/wi223416/dodoni-halloumi-43", "https://www.ah.nl/producten/product/wi142440/ah-maaltijdsalade-italiaanse-kip", "https://www.ah.nl/producten/product/wi233276/pringles-original", "https://www.ah.nl/producten/product/wi365407/ah-dipsalade-couscous-falafel-met-hummus", "https://www.ah.nl/producten/product/wi62761/dr-oetker-big-americans-pizza-texas", "https://www.ah.nl/producten/product/wi191448/granditalia-piccante-met-rode-peper", "https://www.ah.nl/producten/product/wi112744/pepsi-cola-max-4-pack", "https://www.ah.nl/producten/product/wi457669/beyond-meat-the-beyond-burger", "https://www.ah.nl/producten/product/wi196825/granditalia-fusilli-integrali"];
 var productLinks = [];
 var finalProductList = [];
 
@@ -58,22 +59,11 @@ productLinks.forEach(function (element) {
     });
 });
 
-// productLinks.forEach(function(element){
-//     request(element, function (error, response, html) {
-//       if (!error && response.statusCode == 200) {
-//          var obj = JSON.parse(html);
-//          product = {
-//              name: obj._embedded.lanes[4]._embedded.items[0]._embedded.product.description,
-//              image: obj._embedded.lanes[4]._embedded.items[0]._embedded.product.images[0].link.href,
-//              discount: obj._embedded.lanes[4]._embedded.items[0]._embedded.product.discount.label
-//          };
-//          productArray.push(product);
-//       }
-//     });
-// });
+let now = new Date();
+let formattedDate = date.format(now, 'dddd, MMMM DD YYYY');        // => 'Fri Jan 02 2015'
 
 app.get("/", function (req, res) {
-    res.render("landing", { productArray: finalProductList });
+    res.render("landing", { productArray: finalProductList, date: formattedDate });
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
