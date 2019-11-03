cc51e37df5762a107c678de31fb896df82a7b266
mbosselut <duchmol7@hotmail.com>
2019-11-03 15:00:10 +0100

    Experimenting with promises

--- app.js
+++ app.js
@@ -153,26 +153,12 @@ productList = [
     }
 ];
 
-//Old product list
-// var productArray = [
-//     "https://www.ah.nl/producten/product/wi187593/ah-greenfields-jalapeno-burger",
-//     "https://www.ah.nl/producten/product/wi442669/doritos-cool-american-tortilla-chips",
-//     "https://www.ah.nl/producten/product/wi414808/douwe-egberts-lungo-original-koffiecups",
-//     "https://www.ah.nl/producten/product/wi185801/ah-duurzame-vangst-sockeye-zalm",
-//     "https://www.ah.nl/producten/product/wi211029/robijn-klein-and-krachtig-color-wasmiddel",
-//     "https://www.ah.nl/producten/product/wi196835/robijn-wasverzachter-morgenfris",
-//     "https://www.ah.nl/producten/product/wi420319/kwekkeboom-oven-black-angus-bitterballen",
-//     "https://www.ah.nl/producten/product/wi169797/ah-avocado",
-//     "https://www.ah.nl/producten/product/wi223416/dodoni-halloumi-43",
-//     "https://www.ah.nl/producten/product/wi142440/ah-maaltijdsalade-italiaanse-kip",
-//     "https://www.ah.nl/producten/product/wi233276/pringles-original",
-//     "https://www.ah.nl/producten/product/wi365407/ah-dipsalade-couscous-falafel-met-hummus",
-//     "https://www.ah.nl/producten/product/wi62761/dr-oetker-big-americans-pizza-texas",
-//     "https://www.ah.nl/producten/product/wi191448/granditalia-piccante-met-rode-peper",
-//     "https://www.ah.nl/producten/product/wi112744/pepsi-cola-max-4-pack",
-//     "https://www.ah.nl/producten/product/wi457669/beyond-meat-the-beyond-burger",
-//     "https://www.ah.nl/producten/product/wi196825/granditalia-fusilli-integrali"
-// ];
+// printing out specific JSON, occasionnally useful for debugging
+// console.log(
+//     getJSONUrl(
+//         'https://www.ah.nl/producten/product/wi187593/ah-greenfields-jalapeno-burger'
+//     )
+// );
 
 //Translating AH urls to JSON data
 const getJSONUrl = url => {
@@ -182,17 +168,7 @@ const getJSONUrl = url => {
     )}`;
 };
 
-//Previous version :
-// const getJSONUrl = url => {
-//     let obj = {};
-//     const { pathname } = urlTools.parse(url);
-//     obj[0] = url;
-//     obj[1] = `https://www.ah.nl/service/rest/delegate?url=${encodeURIComponent(
-//         pathname
-//     )}`;
-//     return obj;
-// };
-
+// Going through the product list and adding the AH JSON url property
 function extractAHJsons(productList) {
     let productListIncludingJsons = productList.slice();
     productListIncludingJsons.forEach(
@@ -200,133 +176,61 @@ function extractAHJsons(productList) {
     );
     return productListIncludingJsons;
 }
+const productListIncludingJsons = extractAHJsons(productList);
 
-//Previous version:
-// function extractAHJsons(productList) {
-// productList.forEach(element => {
-//     element.JSON = getJSONUrl(element.ahLink);
-// });
-// console.log(productList);
-// return productList;
-//     return productList.map(product => getJSONUrl(product.ahLink));
-// }
+let promise = checkDiscounts(productListIncludingJsons);
 
-// printing out specific JSON
-// console.log(getJSONUrl("https://www.ah.nl/producten/product/wi187593/ah-greenfields-jalapeno-burger"));
-
-// function checkAHdiscounts(productListIncludingJsons) {
-//     let productListAhChecked = [];
-//     productListIncludingJsons.forEach(product => {
-//         // console.log("Element :", product);
-//         request(product.ah.ahJson, function(error, response, html) {
-//             // console.log("reached the request");
-//             if (!error && response.statusCode == 200) {
-//                 var obj = JSON.parse(html);
-//                 if (
-//                     obj._embedded.lanes[4]._embedded.items[0]._embedded.product
-//                         .discount == undefined
-//                 ) {
-//                     // console.log("reached the if");
-//                     product.ah.discount = "No discount";
-//                     product.ah.price =
-//                         obj._embedded.lanes[4]._embedded.items[0]._embedded.product.priceLabel.now;
-//                     productListAhChecked.push(product);
-//                 } else {
-//                     // console.log("reached the else");
-//                     product.ah.discount =
-//                         obj._embedded.lanes[4]._embedded.items[0]._embedded.product.discount.label;
-//                     product.ah.period =
-//                         obj._embedded.lanes[4]._embedded.items[0]._embedded.product.discount.period;
-//                     productListAhChecked.push(product);
-
-//                     // console.log(productListAhChecked);
-//                 }
-//             }
-//         });
-//     });
-//     return productListAhChecked;
-// }
-
-//-----------------ORIGINAL -----------------------//
-// productLinks.forEach(function (element) {
-//     request(element[1], function (error, response, html) {
-//         if (!error && response.statusCode == 200) {
-//             var obj = JSON.parse(html);
-//             if(obj._embedded.lanes[4]._embedded.items[0]._embedded.product.discount == undefined){
-//                 product = {
-//                     name: obj._embedded.lanes[4]._embedded.items[0]._embedded.product.description,
-//                     image: obj._embedded.lanes[4]._embedded.items[0]._embedded.product.images[0].link.href,
-//                     discount: "No discount",
-//                     price: obj._embedded.lanes[4]._embedded.items[0]._embedded.product.priceLabel.now,
-//                     url: element[0]
-//                 };
-//                 finalProductList.push(product);
-//             } else {
-//                 product = {
-//                     name: obj._embedded.lanes[4]._embedded.items[0]._embedded.product.description,
-//                     image: obj._embedded.lanes[4]._embedded.items[0]._embedded.product.images[0].link.href,
-//                     discount: obj._embedded.lanes[4]._embedded.items[0]._embedded.product.discount.label,
-//                     period: obj._embedded.lanes[4]._embedded.items[0]._embedded.product.discount.period,
-//                     url: element[0]
-//                 }
-//                 finalProductList.push(product);
-//             }
-//         }
-//     });
-// });
-
-function checkAHdiscounts(productListIncludingJsons) {}
-
-function checkAHdiscounts(productListIncludingJsons) {
-    let checkedForAHDiscounts = [];
-    productListIncludingJsons.forEach(function(element) {
-        let newObj = Object.assign({}, element);
-        request(newObj.ah.ahJson, function(error, response, html) {
-            if (!error && response.statusCode == 200) {
-                var obj = JSON.parse(html);
-                if (
-                    obj._embedded.lanes[4]._embedded.items[0]._embedded.product
-                        .discount == undefined
-                ) {
-                    newObj.ah.discount = 'No discount';
-                    newObj.ah.price =
-                        obj._embedded.lanes[4]._embedded.items[0]._embedded.product.priceLabel.now;
-                    checkedForAHDiscounts.push(newObj);
-                    // console.log(checkedForAHDiscounts);
-                } else {
-                    newObj.ah.discount =
-                        obj._embedded.lanes[4]._embedded.items[0]._embedded.product.discount.label;
-                    newObj.ah.period =
-                        obj._embedded.lanes[4]._embedded.items[0]._embedded.product.discount.period;
-                    checkedForAHDiscounts.push(newObj);
+// What this function does is go through the whole product list, and fetch the AH discount information for each item.
+function checkDiscounts(productListIncludingJsons) {
+    return new Promise(function(resolve, reject) {
+        productListIncludingJsons.forEach(function(product) {
+            request(product.ah.ahJson, function(error, response, html) {
+                if (!error && response.statusCode == 200) {
+                    var obj = JSON.parse(html);
+                    if (
+                        obj._embedded.lanes[4]._embedded.items[0]._embedded
+                            .product.discount == undefined
+                    ) {
+                        product.ah.discount = 'No discount';
+                        product.ah.price =
+                            obj._embedded.lanes[4]._embedded.items[0]._embedded.product.priceLabel.now;
+                    } else {
+                        product.ah.discount =
+                            obj._embedded.lanes[4]._embedded.items[0]._embedded.product.discount.label;
+                        product.ah.period =
+                            obj._embedded.lanes[4]._embedded.items[0]._embedded.product.discount.period;
+                    }
                 }
-                // console.log("from 1: ", checkedForAHDiscounts);
-                // return checkedForAHDiscounts;
-            }
-            // console.log("from 2: ", checkedForAHDiscounts);
+            });
+            resolve(productListIncludingJsons);
         });
-        console.log('from 3: ', checkedForAHDiscounts);
-        return checkedForAHDiscounts;
     });
-    console.log('from 4: ', checkedForAHDiscounts);
-    return checkedForAHDiscounts;
 }
 
-const productListIncludingJsons = extractAHJsons(productList);
-const finalProductList = checkAHdiscounts(productListIncludingJsons);
-console.log('Final list :', finalProductList);
+// Promise not working, still showing the old array
+promise.then(result => {
+    console.log('Promise completed, final list :', productListIncludingJsons);
+    // Not working either :
+    // console.log('Promise completed, final list :', result);
+});
 
+//When manually checking the productListIncludingJsons AFTER some time, the correct result is printed out. Just a hacky way of
+// checking that the job is done and that the checkDisounts function works properly
+// setTimeout(function() {
+//     console.log('Timeout, final list :', productListIncludingJsons);
+// }, 2000);
+
+//---------------Boring stuff below-----------------//
 let now = new Date();
 let formattedDate = date.format(now, 'dddd, MMMM DD YYYY'); // => 'Fri Jan 02 2015'
 
-// console.log(finalProductList);
-
-// app.get("/", function(req, res) {
-//     res.render("landing", {
-//         productArray: finalProductList,
-//         date: formattedDate
-//     });
-// });
+app.get('/', function(req, res) {
+    res.render('landing', {
+        //replace productArray with productListIncludingJsons when everything works
+        productArray: [],
+        date: formattedDate
+    });
+});
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
     console.log('The AH discounter server has started');
